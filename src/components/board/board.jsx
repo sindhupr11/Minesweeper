d6e8fb8dd5a5cfe711e460fb29f9187488388f91
Sindhu P R <96954675+sindhupr11@users.noreply.github.com>
2024-01-01 14:57:16 +0530

    v2-dev

---
+++ src/components/board/board.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CellContainer } from '../../containers';
+import { Wrapper } from './boardStyle';
+
+const Board = ({ width, height,	onRightClickBoard}) => {
+	return (
+		<Wrapper widthSize={width} onContextMenu={onRightClickBoard}>
+			{Array(width * height).fill().map((v, i) => 
+				<CellContainer key={i} x={i % width} y={Math.floor(i / width)} />
+			)}
+		</Wrapper>
+	);
+};
+
+export default Board;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Board } from './board/board';
+export { default as Cell } from './cell/cell';
+export { default as Settings } from './settings/settings';
+export { default as Status } from './status/status';
